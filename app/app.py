f03311c4b2f71f1c4dcd0c6d60bbcb254348761e
Rama Krishna Reddy <pothamsettiramakrishnareddy@gmail.com>
2026-05-21 11:58:20 +0530

    Fixed dataset path

--- app/app.py
+++ app/app.py
@@ -1,12 +1,19 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import os
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+
+csv_path = os.path.join(BASE_DIR, 'dataset', 'train.csv')
+
+df = pd.read_csv(csv_path)
 
 # Page Title
 st.title("Sales Dashboard")
 
 # Load Dataset
-df = pd.read_csv('../dataset/train.csv')
+df = pd.read_csv('dataset/train.csv')
 
 # Clean Data
 df = df.dropna()
